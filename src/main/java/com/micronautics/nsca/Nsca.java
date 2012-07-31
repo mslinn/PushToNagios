269ea16497408122b93431641e3b6be092d2fa11
Mike Slinn <mslinn@micronauticsresearch.com>
2012-07-31 11:17:36 -0700

    Updated or added copyright notice

--- src/main/java/com/micronautics/nsca/Nsca.java
+++ src/main/java/com/micronautics/nsca/Nsca.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 1999,2004 The Apache Software Foundation.
+ * Portions copyright 2012 Bookish, LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
